6fac259f7849b5b3f31ba6dca8a5cb73d55a0fdc
khaufillah <khaufillahmohammad@gmail.com>
2025-06-10 19:06:44 +0700

    locator init

--- src/test/java/selenium/LocatorSuccessLoginScenario.java
+++ src/test/java/selenium/LocatorSuccessLoginScenario.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -76,7 +78,12 @@ public class LocatorSuccessLoginScenario {
 
         // login
         Thread.sleep(Duration.ofSeconds(2));
+        // loader
         WebElement inputUsername = webDriver.findElement(By.cssSelector("input#inputUsername"));
+
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+        wait.until(t -> ExpectedConditions.visibilityOf(inputUsername));
+
         inputUsername.sendKeys("test@test.com");
 
         Thread.sleep(Duration.ofSeconds(2));
