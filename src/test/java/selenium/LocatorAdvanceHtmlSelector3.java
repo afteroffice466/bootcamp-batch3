8c4ce5672c0de15a76fc8fe1afe5f22732148201
khaufillah <khaufillahmohammad@gmail.com>
2025-06-12 21:40:59 +0700

    day11-filla-selenium-scenario

--- src/test/java/selenium/LocatorAdvanceHtmlSelector3.java
+++ src/test/java/selenium/LocatorAdvanceHtmlSelector3.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
